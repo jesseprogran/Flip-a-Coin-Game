25cb1d8b9869c7c443e1271e5d26293e72c207e4
Jesse Silva <jessepescador22@gmail.com>
2024-03-01 20:39:29 -0300

    começando o javascript e selecionando os elementos do html

---
+++ script.js
@@ -0,0 +1,5 @@
+let heads = 0;
+let tails = 0;
+let coin = document.querySelector(".coin"); // selecionando elemento com classe
+let flipBtn = document.querySelector("#flip-button"); // selecionando elemento com id
+let resetBtn = document.querySelector(".coin"); // selecionando elemento com classe
